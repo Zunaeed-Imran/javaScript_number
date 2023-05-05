7d96a2eaf1ee4a32b7f0b069117d45dadd004b23
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-05 22:24:10 +0600

    Number is positive negative or zero show

---
+++ Positive_Negative.js
@@ -0,0 +1,11 @@
+let n = prompt("Enter: ");
+if (n > 0) {
+  document.write("Positive");
+} else if (n < 0) {
+  document.write("Negative");
+} else if (n == 0) {
+  document.write("Zero");
+}
+else {
+  document.write("Invalid input!");
+}
