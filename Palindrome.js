e96257d0aae949f675fbbce00dc31016a15fb5f7
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-06 14:52:41 +0600

    Palindrome or not in JavaScript

---
+++ Palindrome.js
@@ -0,0 +1,4 @@
+let x = prompt("Enter 1st palindrom Number: ");
+let y = prompt("Enter 1st palindrom Number: ");
+let palindrom = (x * 1001) + (y * 110);
+document.write("Palindrom Number is: "+palindrom);
