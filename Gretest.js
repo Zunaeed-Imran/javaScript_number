439eb12cda90fdb20df26a29eccb4a91c395eb68
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-06 09:53:58 +0600

    Greatest of two Number in JavaScript

---
+++ Gretest.js
@@ -0,0 +1,8 @@
+let m = prompt("Enter  number: ");
+let n = prompt("Enter  number: ");
+
+if (m < n) {
+  document.write(n + " is Gretest");
+} else if (m > n) {
+  document.write(m + " is Gretest");
+}
